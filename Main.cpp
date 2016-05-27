c7a44043e7de9e22b10354b29f5e43852b6db140
jeniby <jeniby16@gmail.com>
2016-05-27 15:18:38 +0200

    Update and rename main.cpp to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <SFML/Graphics.hpp>
+#include <SFML\System.hpp> 
+#include <SFML\Window.hpp>
+#include <iostream>
+#include "view.h"
+#include "Menu.h"
+#include <string>
+#include <sstream>
+#include "file.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "Class.h"
+using namespace sf;
+int main()
+{
+	int z = 0,lvl = 0;
+	gameRunning(&z,&lvl);
+
+	return 0;
+}
