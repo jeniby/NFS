52968a5f7974dbb211f25407e3b4ca934ffa2028
jeniby <jeniby16@gmail.com>
2016-05-27 14:57:04 +0200

    Update Class.h

--- Class.h
+++ Class.h
@@ -5,207 +5,42 @@
 class Player { // класс Игрока
 public:
 
-	Player(String F, float X, float Y, float W, float H)  //Конструктор с параметрами(формальными) для класса Player. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
-	FloatRect getRect() 
-	void update(float time) //функция "оживления" объекта класса. update - обновление. принимает в себя время SFML , вследствие чего работает бесконечно, давая персонажу движение.
-	float getplayercoordinateX() 	//этим методом будем забирать координату Х	
-	float getplayercoordinateY() 	//этим методом будем забирать координату Y 	
+	Player(String F, float X, float Y, float W, float H);  //Конструктор с параметрами(формальными) для класса Player. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
+	FloatRect getRect() ;
+	void update(float time); //функция "оживления" объекта класса. update - обновление. принимает в себя время SFML , вследствие чего работает бесконечно, давая персонажу движение.
+	float getplayercoordinateX(); 	//этим методом будем забирать координату Х	
+	float getplayercoordinateY() ;	//этим методом будем забирать координату Y 	
 };
 class Map {
 public:
-	float x, y, w, h;
-	String File; //файл с расширением
-	Image image;//сфмл изображение
-	Texture texture;//сфмл текстура
-	Sprite sprite;//сфмл спрайт
-	Map(String F, float X, float Y, float W, float H) {  //Конструктор с параметрами(формальными) для класса Player. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
-		File = F;//имя файла+расширение
-		w = W; h = H;//высота и ширина
-		image.loadFromFile("Images/" + File);//запихиваем в image наше изображение вместо File мы передадим то, что пропишем при создании объекта. В нашем случае "hero.png" и получится запись идентичная 	image.loadFromFile("images/hero/png");
-		texture.loadFromImage(image);//закидываем наше изображение в текстуру
-		sprite.setTexture(texture);//заливаем спрайт текстурой
-		x = X; y = Y;//координата появления спрайта
-		sprite.setPosition(x, y);
-	}
+	Map(String F, float X, float Y, float W, float H);  //Конструктор с параметрами(формальными) для класса Player. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
 
 };
 class Box
 {
 public:
-	float x, y, w, h;
-	String File;
-	Image image;
-	Texture texture;
-	Sprite sprite;
-	Box(String F, float W, float H)
-	{
-		File = F;
-		w = W; h = H;
-		image.loadFromFile("Images/" + File);
-		texture.loadFromImage(image);
-		sprite.setTexture(texture);
-		sprite.setPosition(1000, 1000);
-	}
-	FloatRect getRect()
-	{
-		return FloatRect(x, y, w, h);
-	}
+	Box(String F, float W, float H); //конструктор с параметрами(формальными) для класса Box.При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
+	FloatRect getRect(); //возвращает квадрат спрайта ящика
+	void rand_box(float a); //рандомно заполняет дорогу ящиком
 
-	void rand_box(float a)
-	{
-		srand(time(NULL));
-		int k = rand() % 4 + 1;
-		switch (k) {
-		case 1: sprite.setPosition(45, a - 600);
-			x = 45;
-			y = a - 600;
-			break;
-		case 2: sprite.setPosition(150, a - 600);
-			x = 150;
-			y = a - 600;
-			break;
-		case 3: sprite.setPosition(280, a - 600);
-			x = 280;
-			y = a - 600;
-			break;
-		case 4: sprite.setPosition(380, a - 600);
-			x = 380;
-			y = a - 600;
-			break;
-
-		}
-
-	}
 };
 class Sounds
 {
 public:
-	SoundBuffer bufer;
-	Sound s;
-	Sounds(String F)
-	{
-		bufer.loadFromFile("Sounds/" + F);
-		s = Sound(bufer);
-		s.setPlayingOffset(milliseconds(1500));
-	}
+	Sounds(String F); //конструктор для музыки
 };
 class Bot {
 public:
-	float x, y, w, h, dy;
-	String File; //файл с расширением
-	Image image;//сфмл изображение
-	Texture texture;//сфмл текстура
-	Sprite sprite;//сфмл спрайт
-
-	Bot(String F, float X, float Y, float W, float H) {  //Конструктор с параметрами(формальными) для класса Player. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
-		File = F;//имя файла+расширение
-		w = W; h = H;//высота и ширина
-		image.loadFromFile("Images/" + File);//запихиваем в image наше изображение вместо File мы передадим то, что пропишем при создании объекта. В нашем случае "hero.png" и получится запись идентичная 	image.loadFromFile("images/hero/png");
-		image.createMaskFromColor(Color(255, 0, 0));
-		texture.loadFromImage(image);//закидываем наше изображение в текстуру
-		sprite.setTexture(texture);//заливаем спрайт текстурой
-		x = X; y = Y;//координата появления спрайта
-		sprite.setPosition(x, y);
-		dy = ((float)(rand() % 20 + 40)) / 170;
-	}
-	void Rand_Bot(float a, int k)
-	{
-
-		switch (k) {
-		case 1: sprite.setPosition(45, a - 600);
-			x = 46;
-			y = a - 600;
-			break;
-		case 2: sprite.setPosition(150, a - 600);
-			x = 156;
-			y = a - 600;
-			break;
-		case 3: sprite.setPosition(280, a - 600);
-			x = 274;
-			y = a - 600;
-			break;
-		case 4: sprite.setPosition(380, a - 600);
-			x = 374;
-			y = a - 600;
-			break;
-		}
-	}
-	FloatRect getRect()
-	{
-		return FloatRect(x + 5, y + 5, w - 5, h - 5);
-	}
-	void Update(int d)
-	{
-		y -= d*dy * 32;
-		sprite.setPosition(x, y);
-	}
-	void Rand_Color()
-	{
-		srand(time(NULL));
-		int k = rand() % 4 + 1;
-		switch (k)
-		{
-		case 1:
-			sprite.setColor(Color(187, 53, 10));
-			break;
-		case 2:
-			sprite.setColor(Color(107, 186, 0));
-			break;
-		case 3:
-			sprite.setColor(Color(0, 198, 255));
-			break;
-		case 4:
-			sprite.setColor(Color(255, 252, 0));
-			break;
-		}
-	}
+	Bot(String F, float X, float Y, float W, float H); //Конструктор с параметрами(формальными) для класса Bot. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
+	void Rand_Bot(float a, int k); //рандомно расставляе тмашины на дороге
+	FloatRect getRect(); //возвращает квадрат спрайта ботов
+	void Update(int d);  //обновляет координаты
+	void Rand_Color(); //Рандомит цвет
 };
 class Racket
 {
 public:
-	float x, y, w, h;
-	bool life;
-	String File;
-	Image image;
-	Texture texture;
-	Sprite sprite;
-	Racket(String F, float W, float H)
-	{
-		File = F;
-		w = W; h = H;
-		image.loadFromFile("Images/" + File);
-		texture.loadFromImage(image);
-		sprite.setTexture(texture);
-		sprite.setPosition(1000, 1000);
-		life = false;
-	}
-	FloatRect getRect()
-	{
-		return FloatRect(x, y, w, h);
-	}
-
-	void rand_Racket(float a)
-	{
-		int k = rand() % 4 + 1;
-		switch (k) {
-		case 1: sprite.setPosition(45, a - 600);
-			x = 45;
-			y = a - 600;
-			break;
-		case 2: sprite.setPosition(150, a - 600);
-			x = 150;
-			y = a - 600;
-			break;
-		case 3: sprite.setPosition(280, a - 600);
-			x = 280;
-			y = a - 600;
-			break;
-		case 4: sprite.setPosition(380, a - 600);
-			x = 380;
-			y = a - 600;
-			break;
-
-		}
-
-	}
+	Racket(String F, float W, float H); //Конструктор с параметрами(формальными) для класса Racket. При создании объекта класса мы будем задавать имя файла, координату Х и У, ширину и высоту
+	FloatRect getRect(); //возвращает квадрат спрайта ботов
+	void rand_Racket(float a); //Рандомит координаты для ракет
 };
