847fc0f4602cb66185cc721a73f903df5ee495a3
jeniby <jeniby16@gmail.com>
2016-05-27 15:10:02 +0200

    Update file.h

--- file.h
+++ file.h
@@ -2,41 +2,5 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-int sum(FILE * file)
-{
-	int c, x = 0, s = 0, k = 1, n;
-	while ((c = fgetc(file)) != EOF) {
-		n = 1;
-		if ((c >= (int)'0') && (c <= (int)'9')) {
-			if (k == 1) {
-				x += c - (int)'0';
-				k = 0;
-			}
-			else {
-				x *= 10;
-				x += c - (int)'0';
-			}
+int sum(FILE * file) //считывает файл и возвращает сумму цифр
 
-		}
-		else if ((c == (int)'\n') || (c == (int)' ')) {
-			s += x;
-			x = 0;
-			k = 1;
-		}
-		else
-
-			while ((c = fgetc(file)) != EOF) {
-				n = 1;
-				if ((c == (int)'\n') || (c == (int)' ')) {
-					k = 1;
-					x = 0;
-					n = 1;
-					break;
-				}
-			}
-
-	}
-	if ((k == 0) && (n == 1))
-		s += x;
-	return s;
-}
