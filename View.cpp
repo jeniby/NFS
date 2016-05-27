32df8b1f9b37181830332930d0ad3781e37eda1e
jeniby <jeniby16@gmail.com>
2016-05-27 15:12:57 +0200

    Create View.cpp

---
+++ View.cpp
@@ -0,0 +1,6 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+sf::View view;//объявили sfml объект "вид", который и является камерой
+
+void getplayercoordinateforview(float x, float y) //функция для считывания координат игрока
