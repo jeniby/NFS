47497eb34aefbf93e505ea8eb0c9c2ae789700eb
jeniby <jeniby16@gmail.com>
2016-05-29 13:21:08 +0200

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <sstream>
 int sum(FILE * file);
-using namespace sf;
 bool About(RenderWindow & window)// Выводит информацию о игре на экран
 bool LVL(RenderWindow & window,int *lvl)// Выводит выбор уровней после нажатия Play
 void menu(RenderWindow & window, int * lvl)// Выводит все меню на экран
