b7f20a8b21fb200733be69bc40970b1785899d60
jeniby <jeniby16@gmail.com>
2016-05-29 13:52:36 +0200

    Update File.h

--- File.h
+++ File.h
@@ -2,5 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#ifndef FILE_H    // если имя FILE_H ещё не определено
+#define  FILE_H   // определить имя  FILE_H
 int sum(FILE * file) //считывает файл и возвращает сумму цифр
-
+#endif FILE_H     // если  имя FILE_H уже определено, повторно не определять
