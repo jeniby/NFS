0365d1bab9d088a66c9eab533b095c802f102799
jeniby <jeniby16@gmail.com>
2016-06-04 11:55:10 +0200

    Update View.h

--- View.h
+++ View.h
@@ -1,6 +1,7 @@
-#include <SFML/Graphics.hpp>
 #ifndef VIEW_H    // если имя VIEW_H ещё не определено
 #define  VIEW_H   // определить имя  VIEW_H
+#include <SFML/Graphics.hpp>
+
 sf::View view; //объявили sfml объект "вид", который и является камерой
 
 void getplayercoordinateforview(float x, float y) //функция для считывания координат игрока
