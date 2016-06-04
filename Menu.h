879903348d26964aa0e7f4945030e00fe445b403
jeniby <jeniby16@gmail.com>
2016-06-04 15:55:36 +0200

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,5 +1,4 @@
-#ifndef MENU_H    // если имяMENU_H  ещё не определено
-#define  MENU_H   // определить имя  MENU_H 
+#pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <stdlib.h>
@@ -8,4 +7,3 @@ int sum(FILE * file);
 bool About(RenderWindow & window)// Выводит информацию о игре на экран
 bool LVL(RenderWindow & window,int *lvl)// Выводит выбор уровней после нажатия Play
 void menu(RenderWindow & window, int * lvl)// Выводит все меню на экран
-#endif MENU_H    // если  имя MENU_H уже определено, повторно не определять
