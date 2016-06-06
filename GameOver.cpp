866c56cd54c9d5ac570e7296aae470ed8aae3c7a
jeniby <jeniby16@gmail.com>
2016-06-06 22:56:14 +0200

    Update GameOver.cpp

--- GameOver.cpp
+++ GameOver.cpp
@@ -19,12 +19,12 @@ bool GameOver(RenderWindow & window,int *z,int m, int pr)
 	Menu1.setSmooth(true);
 	Menu2.setSmooth(true);
 	//sprite menu
-	Sprite M1(Menu1), M2(Menu2), money4(Money4);
+	Sprite SM1(Menu1), SM2(Menu2), SMoney4(Money4);
 	M1.setTextureRect(IntRect(205, 202, 230, 60));
 	M2.setTextureRect(IntRect(205, 202, 230, 60));
-	money4.setPosition(view.getCenter().x - 135, view.getCenter().y - 80);
-	bool isMenu = 1;
-	int menuNum = 0;
+	SMoney4.setPosition(view.getCenter().x - 135, view.getCenter().y - 80);
+	bool IsMenu = 1;
+	int MenuNum = 0;
 	M1.setOrigin(115, 30);
 	M2.setOrigin(115, 30);
 	//фон
@@ -37,10 +37,10 @@ bool GameOver(RenderWindow & window,int *z,int m, int pr)
 	Money1 << (int)(m);
 	Money2 << (int)(pr);
 	Money3 << (int)(m + pr);
-	money.setString(Money1.str() +" + " + Money2.str() + " = " + Money3.str());
-	money.setPosition(view.getCenter().x - 110, view.getCenter().y - 90);
+	Money.setString(Money1.str() +" + " + Money2.str() + " = " + Money3.str());
+	Money.setPosition(view.getCenter().x - 110, view.getCenter().y - 90);
 
-	while (isMenu)
+	while (IsMenu)
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -59,40 +59,40 @@ bool GameOver(RenderWindow & window,int *z,int m, int pr)
 		//фон
 
 		rectangle.setPosition(view.getCenter().x - 150, view.getCenter().y - 150);
-		menuNum = 0;
+		MenuNum = 0;
 		M1.setScale(1.f, 1.f);
 		M2.setScale(1.f, 1.f);
 		if (IntRect(200, 330, 230, 60).contains(Mouse::getPosition(window)))
 		{
 			M1.setScale(1.05f, 1.05f);
 
-			menuNum = 1;
+			MenuNum = 1;
 		}
 
 		if (IntRect(200, 430, 230, 60).contains(Mouse::getPosition(window)))
 		{
 			M2.setScale(1.05f, 1.05f);
-			menuNum = 2;
+			MenuNum = 2;
 		}
 
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
-			if (menuNum == 1)
+			if (MenuNum == 1)
 			{
 				*z = 1;
 				return true;//если нажали первую кнопку, то выходим из меню 
 			}
-			if (menuNum == 2)
+			if (MenuNum == 2)
 				return true;
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 			return false;
 		window.draw(rectangle);
-		window.draw(M1);
-		window.draw(M2);
-		window.draw(text);
-		window.draw(money);
-		window.draw(money4);
+		window.draw(SM1);
+		window.draw(SM2);
+		window.draw(Text);
+		window.draw(Money);
+		window.draw(SMoney4);
 		window.display();
 	}
 	////////////////////////////////////////////////////
