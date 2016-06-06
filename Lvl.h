c99ced59defa8fffe7eabe4e404ac552fcb4dc65
jeniby <jeniby16@gmail.com>
2016-06-06 23:23:39 +0200

    Update Lvl.h

--- Lvl.h
+++ Lvl.h
@@ -2,4 +2,4 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <stdlib.h>
-bool LVL(RenderWindow & window,int *lvl);// Выводит выбор уровней после нажатия Play
+bool Lvl(RenderWindow & window,int *lvl);// Выводит выбор уровней после нажатия Play
