5d737afb4b4f3c081f57851b8e8e56c88a0bbfd6
jeniby <jeniby16@gmail.com>
2016-06-06 20:27:36 +0200

    Create Music.cpp

---
+++ Music.cpp
@@ -0,0 +1,27 @@
+#include 'Music.h'
+String music()
+{
+	srand(time(NULL));
+	int k = rand() % 6 + 1;
+	switch (k)
+	{
+	case 1:
+		return "Music/1.ogg";
+		break;
+	case 2:
+		return "Music/2.ogg";
+		break;
+	case 3:
+		return "Music/3.ogg";
+		break;
+	case 4:
+		return "Music/4.ogg";
+		break;
+	case 5:
+		return "Music/5.ogg";
+		break;
+	case 6:
+		return "Music/6.ogg";
+		break;
+	}
+}
