2476ff95ac90b27e4a0a5d399aa8a8c21e943b20
jeniby <jeniby16@gmail.com>
2016-06-06 21:04:04 +0200

    Update Game.h

--- Game.h
+++ Game.h
@@ -9,7 +9,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool Escape(RenderWindow & window, int *z); //При нажатии клавиши Escape появляется окно с выбором действий: рестарт, выход в меню.
 bool startGame(int* z,int * lvl); //ф-ция запускает игру lvl передает один из 3 уровней (ящики, встречка, попутка)
 void gameRunning(int *z,int *lvl); //ф-ция перезагружает игру , если это необходимо
-bool startGame(int *z,int*lvl); // сам игровой процесс
+bool startGame(int *z,int* lvl); // сам игровой процесс
