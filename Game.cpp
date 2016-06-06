a7cffed9589e3ac568c45d21770cd9b531fb1964
jeniby <jeniby16@gmail.com>
2016-06-06 21:10:40 +0200

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,105 +1,13 @@
 #include "Game.h"
-bool Escape(RenderWindow & window, int *z)
+bool StartGame(int* z,int * lvl);
+void GameRunning(int *z,int *lvl) //ф-ция перезагружает игру , если это необходимо
 {
-	//создаем тексуры для кнопочек
-	Texture	Menu1, Menu2 ,Menu3,Bg;
-	Menu1.loadFromFile("images/Menu/restart.jpg");
-	Menu2.loadFromFile("images/Menu/menu.jpg");
-	Menu3.loadFromFile("images/Menu/resume.jpg");
-	Bg.loadFromFile("images/Menu/lvl.jpg");
-	Menu1.setSmooth(true);
-	Menu2.setSmooth(true);
-	Menu3.setSmooth(true);
-	//создаем спрайты для кнопок
-	Sprite M1(Menu1), M2(Menu2), M3(Menu3);
-	M1.setTextureRect(IntRect(205, 202, 230, 60));
-	M2.setTextureRect(IntRect(205, 202, 230, 60));
-	M3.setTextureRect(IntRect(205, 202, 230, 60));
-	Menu1.setSmooth(true);
-	bool isMenu = 1;
-	int menuNum = 0;
-	//перемещаем центр тяжести:)
-	M1.setOrigin(115, 30);
-	M2.setOrigin(115, 30);
-	M3.setOrigin(115, 30);
-	//фон
-	RectangleShape rectangle(Vector2f(300, 300));
-	//обводочка
-	rectangle.setOutlineThickness(10);
-	rectangle.setTexture(&Bg);
-	rectangle.setTextureRect(IntRect(0, 200, 520, 520));
-	while (isMenu)
+	if (StartGame(z,lvl))
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-			{
-				window.close();
-				return false;
-			}
-		}
-		//position
-		M1.setPosition(view.getCenter().x, view.getCenter().y);
-		M2.setPosition(view.getCenter().x, view.getCenter().y + 100);
-		M3.setPosition(view.getCenter().x, view.getCenter().y - 100);
-		//фон
-		rectangle.setPosition(view.getCenter().x - 150, view.getCenter().y - 150);
-		menuNum = 0;
-		M1.setScale(1.f, 1.f);
-		M2.setScale(1.f, 1.f);
-		M3.setScale(1.f, 1.f);
-		int n = view.getCenter().x,
-			m = view.getCenter().y;
-			if (IntRect(200, 330, 230, 60).contains(Mouse::getPosition(window)))
-		{
-			M1.setScale(1.05f, 1.05f);
-			menuNum = 1;
-		}
-
-		if (IntRect( 200,  430, 230, 60).contains(Mouse::getPosition(window)))
-		{
-			M2.setScale(1.05f, 1.05f);
-			menuNum = 2;
-		}
-
-		if (IntRect(200, 230, 230, 60).contains(Mouse::getPosition(window)))
-		{
-			M3.setScale(1.05f, 1.05f);
-			menuNum = 3;
-		}
-		if (Mouse::isButtonPressed(Mouse::Left))
-		{
-			if (menuNum == 3)
-				return false;//если нажали первую кнопку, то выходим из меню 
-			if (menuNum == 2)
-				return true;
-			if (menuNum == 1)
-			{
-				*z = 1;
-				return true;
-			}
-		}
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
-			return false;
-		window.draw(rectangle);
-		window.draw(M1);
-		window.draw(M2);
-		window.draw(M3);
-
-		window.display();
-	}
-	////////////////////////////////////////////////////
-}
-bool startGame(int* z,int * lvl);
-void gameRunning(int *z,int *lvl) //ф-ция перезагружает игру , если это необходимо
-{
-	if (startGame(z,lvl))
-	{
-		gameRunning(z,lvl);
+		GameRunning(z,lvl);
 	}////если startGame() == true, то вызываем занова ф-цию isGameRunning, которая в свою очередь опять вызывает startGame() 
 }
-bool startGame(int *z,int*lvl)
+bool StartGame(int *z,int*lvl)
 {
 	ContextSettings settings(0, 0, 8, 1, 1, 0);
 	RenderWindow window(sf::VideoMode(640, 720), "Need for Speed",7U,settings);
