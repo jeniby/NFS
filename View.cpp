fa94548596ebb9abbf7e9e6454e699028951f20d
jeniby <jeniby16@gmail.com>
2016-06-06 22:39:04 +0200

    Update View.cpp

--- View.cpp
+++ View.cpp
@@ -2,7 +2,7 @@
 
 sf::View view;//объявили sfml объект "вид", который и является камерой
 
-void getplayercoordinateforview(float x, float y) { //функция для считывания координат игрока
+void GetPlayerCoordinateForView(float x, float y) { //функция для считывания координат игрока
 
 	view.setCenter(x, y-150); //следим за игроком, передавая его координаты. 
 
