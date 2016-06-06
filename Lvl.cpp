0116a92f96f1ad0fa413b32b14c1951e5c2002b4
jeniby <jeniby16@gmail.com>
2016-06-06 23:05:41 +0200

    Update Lvl.cpp

--- Lvl.cpp
+++ Lvl.cpp
@@ -2,36 +2,36 @@
 bool LVL(RenderWindow & window,int *lvl)
 {
 	// создаю текстуры
-	Texture lvl_Bg, lvl_1, lvl_2,lvl_1r,lvl_2r,Money,Money2,Bk,Bkr,lvl_3,lvl_3r;
+	Texture LvlBg, Lvl1, lvl2,lvlR1,lvlR2,Money,Money2,Bk,BkR,Lvl3,lvlR3;
 	Bk.loadFromFile("images/Menu/Bk.jpg");
-	Bkr.loadFromFile("images/Menu/Bkr.jpg");
-	lvl_Bg.loadFromFile("images/Menu/lvl.jpg");
-	lvl_1.loadFromFile("images/Menu/lvl_1.jpg");
-	lvl_2.loadFromFile("images/Menu/lvl_2.jpg");
-	lvl_1r.loadFromFile("images/Menu/lvl_1r.png");
-	lvl_2r.loadFromFile("images/Menu/lvl_2r.png");
-	lvl_3.loadFromFile("images/Menu/box.png");
-	lvl_3r.loadFromFile("images/Menu/boxr.png");
+	BkR.loadFromFile("images/Menu/Bkr.jpg");
+	LvlBg.loadFromFile("images/Menu/lvl.jpg");
+	Lvl1.loadFromFile("images/Menu/lvl_1.jpg");
+	Lvl2.loadFromFile("images/Menu/lvl_2.jpg");
+	LvlR1.loadFromFile("images/Menu/lvl_1r.png");
+	LvlR2.loadFromFile("images/Menu/lvl_2r.png");
+	Lvl3.loadFromFile("images/Menu/box.png");
+	Lvl3R.loadFromFile("images/Menu/boxr.png");
 	Money.loadFromFile("images/Menu/Money2.png");
 	Money.setSmooth(true);
 	Money2.loadFromFile("images/Menu/Money2.png");
 	Money2.setSmooth(true);
 	// создаю спрайты
-	Sprite lvl_b(lvl_Bg), lvl1(lvl_1), lvl2(lvl_2), lvl1r(lvl_1r), 
-		lvl2r(lvl_2r),money(Money),money2(Money2),bk(Bk),bkr(Bkr),
-		lvl3(lvl_3),lvl3r(lvl_3r);
+	Sprite SLvlB(lvlBg), SLvl1(lvl1), SLvl2(lvl2),SLvl1R(lvlR1), 
+		Slvl2R(lvlR2),SMoney(Money),SMoney2(Money2),SBk(Bk),SBkR(BkR),
+		SLvl3(lvl3),SLvl3r(lvlR3);
 	// позиция спрайта
-	bk.setPosition(0, 0);
-	bkr.setPosition(0, 0);
-	lvl_b.setPosition(0, 0);
-	lvl1.setPosition(130, 400);
-	lvl2.setPosition(370, 400);
-	lvl1r.setPosition(130, 400);
-	lvl2r.setPosition(370, 400);
-	lvl3.setPosition(250,200);
-	lvl3r.setPosition(250, 200);
-	money.setPosition(210,535);
-	money2.setPosition(450, 535);
+	SBk.setPosition(0, 0);
+	SBkr.setPosition(0, 0);
+	SLvl_b.setPosition(0, 0);
+	SLvl1.setPosition(130, 400);
+	SLvl2.setPosition(370, 400);
+	SLvl1r.setPosition(130, 400);
+	SLvl2r.setPosition(370, 400);
+	SLvl3.setPosition(250,200);
+	SLvl3r.setPosition(250, 200);
+	SMoney.setPosition(210,535);
+	SMoney2.setPosition(450, 535);
 	//текст
 	Font font;
 	font.loadFromFile("times.ttf");
@@ -41,9 +41,9 @@ bool LVL(RenderWindow & window,int *lvl)
 	m1.setColor(Color(104, 183, 0));
 	m2.setPosition(390, 530);
 	m2.setColor(Color(104, 183, 0));
-	bool isMenu = 1;
-	int menuNum,n =0, k = 0, l = 0,b = 0;
-	while (isMenu)
+	bool IsMenu = 1;
+	int MenuNum,n =0, k = 0, l = 0,b = 0;
+	while (IsMenu)
 	{
 		Event event;
 		while (window.pollEvent(event))
@@ -54,48 +54,48 @@ bool LVL(RenderWindow & window,int *lvl)
 				return false;
 			}
 		}
-		menuNum = 0;
+		MenuNum = 0;
 		n = 0;
 		k = 0;
 		l = 0;
 		b = 0;
 		if (IntRect(130, 400, 139, 135).contains(Mouse::getPosition(window)))
 		{
-			menuNum = 1;
+			MenuNum = 1;
 			n = 1;
 		}
 		if (IntRect(370, 400, 139, 135).contains(Mouse::getPosition(window)))
 		{
-			menuNum = 2;
+			MenuNum = 2;
 			k = 1;
 		}
 		if (IntRect(0, 0, 87, 50).contains(Mouse::getPosition(window)))
 		{
-			menuNum = 3;
+			MenuNum = 3;
 			l = 1;
 		}
 		if (IntRect(250,200, 139, 135).contains(Mouse::getPosition(window)))
 		{
-			menuNum = 4;
+			MenuNum = 4;
 			b = 1;
 		}
 		if (Mouse::isButtonPressed(Mouse::Left))
 		{
-			if (menuNum == 1)
+			if (MenuNum == 1)
 			{
 				*lvl = 1;
 				return true;
 			}//если нажали первую кнопку, то выходим из меню 
-			if (menuNum == 2)
+			if (MenuNum == 2)
 			{
 				*lvl = 2;
 				return true;
 			}
-			if (menuNum == 3)
+			if (MenuNum == 3)
 			{
 				return false;
 			}
-			if (menuNum == 4)
+			if (MenuNum == 4)
 			{
 				*lvl = 3;
 				return true;
@@ -103,47 +103,47 @@ bool LVL(RenderWindow & window,int *lvl)
 
 		}
 		
-		window.draw(lvl_b);
+		window.draw(SLvlB);
 		if (n == 0)
 		{
-			window.draw(lvl1);
+			window.draw(SLvl1);
 			
 		}
 		else
 		{
-			window.draw(lvl1r);
+			window.draw(SLvl1R);
 		}
 		if (k == 0)
 		{
-			window.draw(lvl2);
+			window.draw(SLvl2);
 
 		}
 		else
 		{
-			window.draw(lvl2r);
+			window.draw(SLvl2R);
 		}
 		if (l == 0)
 		{
-			window.draw(bk);
+			window.draw(SBk);
 
 		}
 		else
 		{
-			window.draw(bkr);
+			window.draw(SBkR);
 		}
 		if (b == 0)
 		{
-			window.draw(lvl3);
+			window.draw(SLvl3);
 
 		}
 		else
 		{
-			window.draw(lvl3r);
-		}
+			window.draw(SLvl3R);
+		
 		window.draw(m1);
 		window.draw(m2);
-		window.draw(money);
-		window.draw(money2);
+		window.draw(SMoney);
+		window.draw(SMoney2);
 		window.display();
 		
 	}
