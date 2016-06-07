ad1ecd1745bc6580dba7ad1ebd597ae2caba57cf
jeniby <jeniby16@gmail.com>
2016-06-07 12:11:29 +0200

    Update Game.h

--- Game.h
+++ Game.h
@@ -8,7 +8,13 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "View.h"
+#include "Menu.h"
+#include "File.h"
+#include "Class.h"
+#include "Music.h"
+#include "GameOver.h"
+#include "Escape.h"
 bool StartGame(int* z,int * lvl); //ф-ция запускает игру lvl передает один из 3 уровней (ящики, встречка, попутка)
 void GameRunning(int *z,int *lvl); //ф-ция перезагружает игру , если это необходимо
 bool StartGame(int *z,int* lvl); // сам игровой процесс
